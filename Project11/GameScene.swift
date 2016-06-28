f71a342b62af72651df86aef38207e928868533d
sachin lamba <sachin.lamba@moldedbits.com>
2016-06-28 15:43:04 +0530

    Add obstacles in screen and play.

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -5,10 +5,29 @@
 //  Created by Sachin Lamba on 28/06/16.
 //  Copyright (c) 2016 LambaG. All rights reserved.
 //
-
+import GameplayKit
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    var editLabel: SKLabelNode!
+    var scoreLabel: SKLabelNode!
+    
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editLabel.text = "Done"
+            }
+            else {
+                editLabel.text = "Edit"
+            }
+        }
+    }
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -30,21 +49,54 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeBouncerAt(CGPoint(x: 512, y: 0))
         makeBouncerAt(CGPoint(x: 768, y: 0))
         makeBouncerAt(CGPoint(x: 1024, y: 0))
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .Right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+        
+        editLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editLabel.text = "Edit"
+        editLabel.position = CGPoint(x: 80, y: 700)
+        addChild(editLabel)
 }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         if let  touch = touches.first {
-            let location = touch.locationInNode(self)
-            let ball = SKSpriteNode(imageNamed: "ballRed")
-            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2.0)
-            //passing collision info
-            ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
             
-            ball.physicsBody!.restitution = 0.4
-            ball.position = location
-            ball.name = "ball"
-            addChild(ball)
+            let location = touch.locationInNode(self)
+            
+            let objects = nodesAtPoint(location) as [SKNode]
+            
+            if objects.contains(editLabel) {
+                editingMode = !editingMode
+            } else {
+                if editingMode {
+                    let size = CGSize(width: GKRandomDistribution(lowestValue: 16, highestValue: 128).nextInt(), height: 16)
+                    let box = SKSpriteNode(color: RandomColor(), size: size)
+                    box.zRotation = RandomCGFloat(min: 0, max: 3)
+                    box.position = location
+                    
+                    box.physicsBody = SKPhysicsBody(rectangleOfSize: box.size)
+                    box.physicsBody!.dynamic = false
+                    
+                    addChild(box)
+                }
+                else {
+                    
+                    let ball = SKSpriteNode(imageNamed: "ballRed")
+                    ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2.0)
+                    //passing collision info
+                    ball.physicsBody!.contactTestBitMask = ball.physicsBody!.collisionBitMask
+            
+                    ball.physicsBody!.restitution = 0.4
+                    ball.position = location
+                    ball.name = "ball"
+                    addChild(ball)
+                }
+            }
         }
     }
    
@@ -92,8 +144,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func collisionBetweenBall(ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroyBall(ball)
+            score += 1
         } else if object.name == "bad" {
             destroyBall(ball)
+            score -= 1
         }
     }
     
