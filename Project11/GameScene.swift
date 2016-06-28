bc8c83c73e98f41a2692ce9401406e72669a216f
sachin lamba <sachin.lamba@moldedbits.com>
2016-06-28 13:42:59 +0530

    Add slots for ball to collide and bounce back.Define slots properties.

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -8,7 +8,8 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let background = SKSpriteNode(imageNamed: "background.jpg")
@@ -16,14 +17,75 @@ class GameScene: SKScene {
         background.blendMode = .Replace
         background.zPosition = -1
         addChild(background)
-    }
+        physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        physicsWorld.contactDelegate = self
+        
+        makeSlotAt(CGPoint(x: 128, y: 0), isGood: true)
+        makeSlotAt(CGPoint(x: 384, y: 0), isGood: false)
+        makeSlotAt(CGPoint(x: 640, y: 0), isGood: true)
+        makeSlotAt(CGPoint(x: 896, y:0), isGood: false)
+        
+        makeBouncerAt(CGPoint(x: 0, y: 0))
+        makeBouncerAt(CGPoint(x: 256, y: 0))
+        makeBouncerAt(CGPoint(x: 512, y: 0))
+        makeBouncerAt(CGPoint(x: 768, y: 0))
+        makeBouncerAt(CGPoint(x: 1024, y: 0))
+}
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
-        
+        if let  touch = touches.first {
+            let location = touch.locationInNode(self)
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width/2.0)
+            
+            ball.physicsBody!.restitution = 0.4
+            ball.position = location
+            ball.name = "ball"
+            addChild(ball)
+        }
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+    
+    func makeBouncerAt(position: CGPoint) {
+        let bouncher = SKSpriteNode(imageNamed: "bouncer")
+        bouncher.position = position
+        bouncher.physicsBody = SKPhysicsBody(circleOfRadius: bouncher.size.width/2.0)
+        bouncher.physicsBody!.dynamic = false
+        addChild(bouncher)
+    }
+    
+    func makeSlotAt(position: CGPoint, isGood: Bool) {
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+        
+        if isGood {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+            slotBase.name = "good"
+        }
+        else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+            slotBase.name = "bad"
+        }
+        
+        slotBase.position = position
+        slotGlow.position = position
+        slotBase.physicsBody = SKPhysicsBody(rectangleOfSize: slotBase.size)
+        slotBase.physicsBody!.dynamic = false
+        
+        addChild(slotBase)
+        addChild(slotGlow)
+        
+        let spin = SKAction.rotateByAngle(CGFloat(M_PI_2), duration: 10)
+        let spinForever = SKAction.repeatActionForever(spin)
+        slotGlow.runAction(spinForever)
+    }
+    
+    
+    
 }
